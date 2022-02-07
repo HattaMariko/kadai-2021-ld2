bfe37aebf80f0b86ca031ea620234ed9139d39a2
Kamata, Ryo <mugil.cephalus+github@gmail.com>
2022-02-07 19:20:25 +0900

    Update index1.js
    素数の判定方法が間違っているようです。
    元のプログラムに、例えば 9 を入力すると「素数である」と出力されます。
    「エラトステネスのふるい」で調べてみてください。

--- kadai-03/index1.js
+++ kadai-03/index1.js
@@ -1,11 +1,15 @@
 const n = parseInt(process.argv[2])
+
+let isPrime = true
+
 for(let i = 2; i < n; i = i +1){
     if(n % i == 0){
         console.log(`${n}は素数でない`)
+        isPrime = false
         break        
-    }else{
-        console.log(`${n}は素数である`)
-        break
     }
 }
 
+if(isPrime) {
+  console.log(n)
+}
